46e0f4dd0759b5e712a80a352e4d521f9b6b26b0
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-05 11:06:21 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,19 +1,4 @@
-#! python3
-"""
-Ask the user to enter a number. 
-Tell them if the number is a positive integer
-(2 points) 
 
-inputs:
-a number of any type
-
-outputs:
-xx is a positive integer.
-xx is not a positive integer
-
-example:
-Enter a number: -3
--3 is not a positive integer
 
 a=input("Enter a number")
 a=int(a)
