741a01b3e4c6031fc3963d8a1ba7d7cde8d37f8e
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-06 11:04:10 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -48,6 +48,6 @@ elif c>(a and b):
     z=math.pow(c,2)
 
 if z==(y+x):
-    print(" form a Pythagorean triple")
+    print(y,+x,+z+" form a Pythagorean triple")
 elif z<(y+z) or z>(y+x):
     print(" do not form a Pythagorean triple")
